fe367ad9616db2e64437a57cbeaa0edfcd237bdc
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-08-19 09:55:17 +0200

    user improvements

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 
+function handleError(inner) {
+  return () =>
+    Promise.resolve(inner()).catch((err) => {
+      console.error(err);
+      alert(err.message);
+    });
+}
+
+document.querySelector("#connect").addEventListener(
+  "click",
+  handleError(async () => {
+    await ethereum.request({ method: "eth_requestAccounts" });
+  }),
+);
+
 const ORDER_TYPE = [
   { name: "sellToken", type: "address" },
   { name: "buyToken", type: "address" },
@@ -16,38 +31,66 @@ const ORDER_TYPE = [
   { name: "buyTokenBalance", type: "string" },
 ];
 
-document.querySelector("#connect").addEventListener("click", async () => {
-  await ethereum.request({ method: "eth_requestAccounts" });
-});
+const NETWORKS = {
+  1: "mainnet",
+  4: "rinkeby",
+  100: "xdai",
+};
 
-document.querySelector("#sign").addEventListener("click", async () => {
-  const { chainId } = await provider.getNetwork();
-  const order = {
-    sellToken: document.querySelector("#sellToken").value,
-    buyToken: document.querySelector("#buyToken").value,
-    receiver: document.querySelector("#receiver").value,
-    sellAmount: document.querySelector("#sellAmount").value,
-    buyAmount: document.querySelector("#buyAmount").value,
-    validTo: document.querySelector("#validTo").value,
-    appData: document.querySelector("#appData").value,
-    feeAmount: document.querySelector("#feeAmount").value,
-    kind: document.querySelector("#kind").value,
-    partiallyFillable: document.querySelector("#partiallyFillable").value,
-    sellTokenBalance: document.querySelector("#sellTokenBalance").value,
-    buyTokenBalance: document.querySelector("#buyTokenBalance").value,
-  };
-  console.log(order);
-  const signature = await signer._signTypedData(
-    {
-      name: "Gnosis Protocol",
-      version: "v2",
-      chainId,
-      verifyingContract: "0x9008D19f58AAbD9eD0D60971565AA8510560ab41",
-    },
-    { Order: ORDER_TYPE },
-    order,
-  );
-  console.log("Asdfasdf");
-  const { v, r, s } = ethers.utils.splitSignature(signature);
-  console.log({ v: `0x${v.toString(16)}`, r, s });
-});
+document.querySelector("#sign").addEventListener(
+  "click",
+  handleError(async () => {
+    const { chainId } = await provider.getNetwork();
+    const network = NETWORKS[chainId];
+    if (network === undefined) {
+      throw new Error(`unsupported network ${chainId}`);
+    }
+
+    const order = {
+      sellToken: document.querySelector("#sellToken").value,
+      buyToken: document.querySelector("#buyToken").value,
+      receiver: document.querySelector("#receiver").value,
+      sellAmount: document.querySelector("#sellAmount").value,
+      buyAmount: document.querySelector("#buyAmount").value,
+      validTo: parseInt(document.querySelector("#validTo").value),
+      appData: document.querySelector("#appData").value,
+      feeAmount: document.querySelector("#feeAmount").value,
+      kind: document.querySelector("#kind").value,
+      partiallyFillable: document.querySelector("#partiallyFillable").checked,
+      sellTokenBalance: document.querySelector("#sellTokenBalance").value,
+      buyTokenBalance: document.querySelector("#buyTokenBalance").value,
+    };
+    const signature = await signer._signTypedData(
+      {
+        name: "Gnosis Protocol",
+        version: "v2",
+        chainId,
+        verifyingContract: "0x9008D19f58AAbD9eD0D60971565AA8510560ab41",
+      },
+      { Order: ORDER_TYPE },
+      order,
+    );
+
+    const response = await fetch(
+      `https://protocol-${network}.dev.gnosisdev.com/api/v1/orders`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...order,
+          signature,
+          signingScheme: "eip712",
+          from: await signer.getAddress(),
+        }),
+      },
+    );
+    const body = await response.json();
+    if (!response.ok) {
+      throw new Error(body.description);
+    }
+
+    alert(`https://protocol-explorer.dev.gnosisdev.com/orders/${body}`);
+  }),
+);
