f4bc0dfb96bbdcc7c8d297b0b1ba17d997540ced
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-08-31 14:06:28 +0200

    signature support

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
+const settlement = new ethers.Contract(
+  "0x9008D19f58AAbD9eD0D60971565AA8510560ab41",
+  [
+    "function setPreSignature(bytes orderUid, bool signed)",
+  ],
+  signer,
+);
 
 function handleError(inner) {
   return () =>
@@ -8,13 +15,6 @@ function handleError(inner) {
       alert(err.message);
     });
 }
-
-document.querySelector("#connect").addEventListener(
-  "click",
-  handleError(async () => {
-    await ethereum.request({ method: "eth_requestAccounts" });
-  }),
-);
 
 const ORDER_TYPE = [
   { name: "sellToken", type: "address" },
@@ -40,12 +40,20 @@ const NETWORKS = {
 document.querySelector("#sign").addEventListener(
   "click",
   handleError(async () => {
+    await ethereum.request({ method: "eth_requestAccounts" });
+
     const { chainId } = await provider.getNetwork();
     const network = NETWORKS[chainId];
     if (network === undefined) {
       throw new Error(`unsupported network ${chainId}`);
     }
 
+    const domain = {
+      name: "Gnosis Protocol",
+      version: "v2",
+      chainId,
+      verifyingContract: "0x9008D19f58AAbD9eD0D60971565AA8510560ab41",
+    };
     const order = {
       sellToken: document.querySelector("#sellToken").value,
       buyToken: document.querySelector("#buyToken").value,
@@ -60,16 +68,32 @@ document.querySelector("#sign").addEventListener(
       sellTokenBalance: document.querySelector("#sellTokenBalance").value,
       buyTokenBalance: document.querySelector("#buyTokenBalance").value,
     };
-    const signature = await signer._signTypedData(
-      {
-        name: "Gnosis Protocol",
-        version: "v2",
-        chainId,
-        verifyingContract: "0x9008D19f58AAbD9eD0D60971565AA8510560ab41",
-      },
-      { Order: ORDER_TYPE },
-      order,
-    );
+    const signingScheme = document.querySelector("#signingScheme").value;
+
+    let signature;
+    switch (signingScheme) {
+      case "eip712":
+        signature = await signer._signTypedData(
+          domain,
+          { Order: ORDER_TYPE },
+          order,
+        );
+        break;
+      case "ethsign":
+        signature = await signer.signMessage(
+          ethers.utils.arrayify(
+            ethers.utils._TypedDataEncoder.hash(
+              domain,
+              { Order: ORDER_TYPE },
+              order,
+            ),
+          ),
+        );
+        break;
+      case "presign":
+        signature = (await signer.getAddress()).toLowerCase();
+        break;
+    }
 
     const response = await fetch(
       `https://protocol-${network}.dev.gnosisdev.com/api/v1/orders`,
@@ -81,7 +105,7 @@ document.querySelector("#sign").addEventListener(
         body: JSON.stringify({
           ...order,
           signature,
-          signingScheme: "eip712",
+          signingScheme,
           from: await signer.getAddress(),
         }),
       },
@@ -89,6 +113,24 @@ document.querySelector("#sign").addEventListener(
     const body = await response.json();
     if (!response.ok) {
       throw new Error(body.description);
+    }
+
+    if (signingScheme === "presign") {
+      await settlement.setPreSignature(
+        ethers.utils.solidityPack(
+          ["bytes32", "address", "uint32"],
+          [
+            ethers.utils._TypedDataEncoder.hash(
+              domain,
+              { Order: ORDER_TYPE },
+              order,
+            ),
+            await signer.getAddress(),
+            order.validTo,
+          ],
+        ),
+        true,
+      );
     }
 
     alert(`https://protocol-explorer.dev.gnosisdev.com/orders/${body}`);
